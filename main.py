c607d2ef40a082555cb5f9fdc29fde6ab04357bf
LENOVO USER <talyagross@gmail.com>
2022-10-01 23:36:46 +0300

    new version

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+"""
+Author: Talya Gross
+Object Oriented Programming- project
+"""
 from shape_storage import *
-
+from sqaure import Sqaure
 
 my_container = ShapeStorage()
 my_container.generate(100)
 print("total area:", my_container.sum_areas())
 print("total perimeter:", my_container.sum_perimeter())
 print("colors:", my_container.count_colors())
+
+rectangle = Rectangle('red', 2, 4)
+square = Sqaure('red', 2, 6)
+print(rectangle.add(square).get_width())
