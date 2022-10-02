e0b3dcd392256baff493d586e823b2300c854350
LENOVO USER <talyagross@gmail.com>
2022-10-02 16:55:58 +0300

    new version

--- shape.py
+++ shape.py
@@ -3,18 +3,28 @@ import abc
 
 class Shape:
 
-    def __init__(self, color=0):
+    def __init__(self, color=''):
         """
-        constructor
+        the constructor function
         :param color: the color of the shape
         """
+
         self.__color = color
 
     def set_color(self, color):
+        """
+        changing the color of the shape
+        :param color: the color of the shape
+        """
         color = self.__color
 
     def get_color(self):
+        """
+        :return: the color of the shape
+        """
         return self.__color
+
+    # empty functions
 
     @abc.abstractmethod
     def get_area(self):
@@ -23,3 +33,12 @@ class Shape:
     @abc.abstractmethod
     def get_perimeter(self):
         pass
+
+
+def main():
+    element = Shape('red')
+    assert element.get_color() == 'red'
+
+
+if __name__ == '__main__':
+    main()
